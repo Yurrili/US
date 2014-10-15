ea24e3e01b711fc5be04d37cd23c31c1b6342648
Yurrili <yurrili931@gmail.com>
2014-10-15 22:31:31 +0200

    Another update

--- Class-1.c
+++ Class-1.c
@@ -1,54 +1,54 @@
-/*
- * GccApplication1.c
- *
- * Created: 2014-10-15 16:20:46
- *  Author:  Mateusz Utkala, Oleksandr Kuzhel, Klaudia Olejniczak
- */ 
- 
+Ôªø/*
+* GccApplication1.c
+*
+* Created: 2014-10-15 16:20:46
+*  Author:  Mateusz Utkala, Oleksandr Kuzhel, Klaudia Olejniczak
+*/
 
-//Miarowe przygasanie i zapalanie diod
 
-#define F_CPU 16000000UL // symbol zawierajπcy czÍstotliwoúÊ taktowania procesora
-#define swieci 0b11111111 
-#define ciemne 0b00000000
+//Stopniowe przygasanie i zapalanie diod
+
+#define F_CPU 16000000UL // definicja szybko≈õci taktowania procesora potrzebna funkcji dely do poprawnej pracy
+#define swieci 0b11111111 //ustawienie bit√≥w na 1 powoduje zapalenie di√≥d
+#define ciemne 0b00000000 //- = - wygaszenie di√≥d
 #include <avr/io.h>
 #include <stdint.h>
 #include <util/delay.h>
 
 int main(void)
 {
-    DDRA = 0xFF;
-    int i ;
-    int temp = 100;
-    int j = 1;
-    while(1)
-    {
-           
-        PORTA = ciemne;
-        
-        for (i=1;i<1000;i++) //petla zapalajπca stopniowo diody
-        {
-            
-            PORTA = swieci;
-            _delay_ms(i/100); //opÛünienie zegara   
+	DDRA = 0xFF; // ustawienie porty A na output
+	int i;
+	int temp = 100;
+	int j = 1;
+	while (1)
+	{
 
-        
-            PORTA = ciemne;
-            _delay_ms(10-i/100);
-        }
-        
-        PORTA = swieci;
-        
-        for (i=1;i<1000;i++) // petla gaszaca stopniowo diody
-        {
-            
-            PORTA = ciemne;
-            _delay_ms(i/100);
+		PORTA = ciemne;  // manipulacja wyj≈õciem portu A 
 
-            
-            PORTA = swieci;
-            _delay_ms(10-i/100);
-        }
-   
-    }
+		for (i = 1; i<1000; i++) //petla zapalaj¬πca stopniowo diody
+		{
+
+			PORTA = swieci;
+			_delay_ms(i / 100); //op√≥≈∏nienie zegara   
+
+
+			PORTA = ciemne;
+			_delay_ms(10 - i / 100);
+		}
+
+		PORTA = swieci;
+
+		for (i = 1; i<1000; i++) // petla gaszaca stopniowo diody
+		{
+
+			PORTA = ciemne;
+			_delay_ms(i / 100);
+
+
+			PORTA = swieci;
+			_delay_ms(10 - i / 100);
+		}
+
+	}
 }
