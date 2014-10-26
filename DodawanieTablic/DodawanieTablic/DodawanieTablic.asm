b7d32f72206006e3b7222eb8ed8fabf04c099306
DonMat <acemateusza@gmail.com>
2014-10-26 02:35:05 +0200

    Beta 1
    Chyba dziala ale jeszcze wymaga poprawek

--- DodawanieTablic/DodawanieTablic/DodawanieTablic.asm
+++ DodawanieTablic/DodawanieTablic/DodawanieTablic.asm
@@ -5,23 +5,79 @@
  */ 
 
 
- .include  "m32def.inc"; wczytuje sterowanie, nazwy portow etc. kontrolera 
+.include  "m32def.inc"; wczytuje sterowanie, nazwy portow etc. kontrolera 
 .EQU LEN = 16 ;dlugosc tablic
-.DSEG
-.org 0x01FF;
-tab1: .BYTE LEN
-tab2: .BYTE LEN
+.DSEG ;segment danych, twozymy dwie tablice o zdefiniowanej dlugosci w LEN
+.org 0x60 ;ustalenie pierwszego adresu segmentu danych
+	tab1: .BYTE LEN
+	tab2: .BYTE LEN
 
 .CSEG ;ustalamy segment kodu
 .org 0; zaczynamy od adresu zero
- 
-;tab1
-LDI XL , low(tab1);mniej znaczacy bit
-LDI XH , high(tab1);bardziej znaczacy bit
-;tab2 
-LDI ZL , low(tab2);mniej znaczacy bit
-LDI ZH , high(tab2);bardziej znaczacy bit 
+	.DEF INDEX = R16 ; definiujemy zmienna index jako R16
+	.DEF TEMP = R17
 
-CLC
-ADC ZL,XL
-ADC ZH,XH
+	;tab1
+	
+	LDI XL , low(tab1) ;mniej znaczacy bit - wskaŸnik
+	LDI XH , high(tab1) ;bardziej znaczacy bit
+	;tab2 
+	LDI YL , low(tab2)
+	LDI YH , high(tab2)
+
+	LDI INDEX , LEN ; index tablicy ustawiam na d³ugoœæ i bêdziemy zmniejszaæ do 0 w pêtli LOOP
+
+
+
+	LOOP:
+		LD TEMP , X
+		LD R18 , Y+
+
+		ADC R18 , TEMP
+		st X+ , R18
+		BRCS DODAJ
+
+
+	PO_DODANIU:
+		DEC INDEX
+	BRNE LOOP	; jump if not ZERO flag, gdy flaga zero w INDEX != 0 wykonaj pêtle jeszcze raz
+	JMP DALEJ
+
+	DODAJ:
+		LD R19 , X
+		INC R19
+		st X , R19
+		BRCS DODAJ
+		JMP PO_DODANIU
+
+	DALEJ:
+		NOP
+
+
+	/* opcja 1.0
+	
+
+	ADD XL , INDEX	;przesuwamy wskaŸnik na ostatni¹ pozycjê tablicy
+	ADD YL , INDEX
+
+	LOOP:
+	 LD TEMP , X
+	 
+	 DEC XL ; przesuwam wskaŸnik o jeden w lewo
+	 DEC INDEX ; wartownik dla pêtli
+	BRNE LOOP	; jump if not ZERO flag, gdy flaga zero w INDEX != 0 wykonaj pêtle jeszcze raz
+	*/
+	
+
+/*	opcja 0.5
+LDI INDEX , LEN
+	ADD_LOOP:
+	LD R20 , -X
+	LD R21 , -Y
+
+	ADC R26 , R20
+	ADC X , R21
+
+	DEC INDEX
+	BRNE ADD_LOOP
+	*/
