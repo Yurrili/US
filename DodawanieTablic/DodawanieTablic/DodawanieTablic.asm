55953ba977c84f1efff9cbe9fdacbe0f761787e0
DonMat <acemateusza@gmail.com>
2014-10-26 15:07:42 +0100

    Działa od LEWEJ do PRAWEJ
    Program czyta mlodsze bity tablic od lewej do prawej

--- DodawanieTablic/DodawanieTablic/DodawanieTablic.asm
+++ DodawanieTablic/DodawanieTablic/DodawanieTablic.asm
@@ -2,6 +2,7 @@
  * DodawanieTablic.asm
  *
  *   Author:  Mateusz Utkala, Oleksandr Kuzhel, Klaudia Olejniczak
+ *	 Program czyta mlodsze bity tablic od lewej do prawej
  */ 
 
 
@@ -17,7 +18,7 @@
 	.DEF INDEX = R16				;definiujemy zmienna index jako R16
 	.DEF TEMP = R17	
 	.DEF TMP = R18
-	.DEF wLEWO = R21				;ilosc przesuniec sie w lewo przy przenoszeniu Carry
+	.DEF wPRAWO = R21				;ilosc przesuniec sie w prawo przy przenoszeniu Carry
 
 	;tab1	
 	LDI XL , low(tab1)				;wskaŸnik na pierwszy element
@@ -25,47 +26,46 @@
 	;tab2 
 	LDI YL , low(tab2)
 	LDI YH , high(tab2)
-
-	LDI INDEX , (LEN-1)				;index tablicy ustawiamy na d³ugoœæ -1 i bêdziemy zmniejszaæ do 0 w pêtli LOOP. 0 warunkiem stopu
-
-	ADD XL , INDEX					;przesuwamy wskaŸniki X i Y na koniec tablicy
-	ADD YL , INDEX
-	INC INDEX						;poprawa indeksu aby wykona³a siê odpowiednia iloœæ iteracji
+	
+	LDI INDEX , LEN 				;index tablicy ustawiamy na d³ugoœæ i bêdziemy zmniejszaæ do 0 w pêtli LOOP. 0 warunkiem stopu
 
 	LOOP:
-		LD TEMP , X					;wczytujemy kolejne elementy rejestru indeksowanego X i Y
-		LD TMP , Y
+		LD TEMP , X					;wczytujemy kolejne elementy rejestru indeksowanego X i Y+ (X zostaje a Y siê przesuwa)
+		LD TMP , Y+
 
 		ADD TMP , TEMP
-		st X , TMP					;zapisujemy wartosc dodawania w odpowiedniej komórce rejestru X
-		DEC XL						;przesuwamy wskaŸniki w lewo
-		DEC YL
-		LDI wLEWO , 0				;jezeli pojawi siê przeniesienie bêdzie potrzebne nadmiarowe przejœcie w lewo wiêc trzeba zliczyæ ile tego bêdzie
+		st X+ , TMP					;zapisujemy wartosc dodawania w odpowiedniej komórce rejestru X i przesuwamy go w prawo
+		LDI wPRAWO , 0				;jezeli pojawi siê przeniesienie bêdzie potrzebne nadmiarowe przejœcie w prawo wiêc trzeba zliczyæ ile tego bêdzie
 		BRCS DODAJ					;wyst¹pi³o przeniesienie przechodzimy do poprawienia wartoœci
 
 	PO_DODANIU:
 		DEC INDEX					;zmniejszamy iloœæ pozosta³ych elementów
 
 	BRNE LOOP						;jump if not ZERO flag, gdy flaga zero w INDEX != 0 wykonaj pêtle jeszcze raz
-	JMP DALEJ						;koniec elementów wiêc koñczymy 
+	JMP KONIEC						;koniec elementów wiêc koñczymy 
 
 	DODAJ:
-		CLC							;trzeba wyczyœciæ flagê C bo tu mo¿e znów wyst¹piæ
+		CLC							;trzeba wyczyœciæ flagê C bo tu mo¿e znów wyst¹piæ przeniesienie
+		INC wPRAWO					;zliczamy przesuniêcie
 		LD R19 , X					;odczytujemy element na lewo od pola gdzie by³o przeniesienie
-		LDI R20 , 1					;INC R19
+		LDI R20 , 1					;< == INC R19
 		ADD R19, R20				;dodawanie zamiast INC bo INC nie zmienia flagi C
 		st X , R19					;zapis wartoœci poprawionej o przeniesienie
-		BRCS LEWO					;by³o przeniesienie
+		BRCS PRAWO					;by³o przeniesienie, trzeba przeskoczyæ do nastêpnej komórki
 		BRCC POPRAW					;nie by³o przeniesienia, trzeba poprawiæ ewentualne przejœcia w lewo
 
-	LEWO:
-		DEC XL						;wskaŸnik X w lewo
-		INC wLEWO					;zliczamy przesuniêcie
+	PRAWO:
+		LD R19 , X+ 				;wskaŸnik X w prawo
 		JMP DODAJ					;bezwarunkowo skaczemy do poprawy wartoœci
 
-	POPRAW:
-		ADD XL , wLEWO				;przesuwamy wskaŸnik X w miejsce ostatniej operacji
+	POPRAW:					
+		DEC wPRAWO					
+		BRNE PRZESUN				;przesuwamy wskaŸnik X w miejsce ostatniej operacji
 		JMP PO_DODANIU				;wracamy do g³ównej pêtli bo wskaŸniki s¹ poprawne
-		
-	DALEJ:
+
+	PRZESUN:
+		LD R19 , -X					;cofamy X o jedn¹komórkê w lewo
+		JMP POPRAW
+
+	KONIEC:
 		NOP							;aby nie robic BREAK 
