e7d0ab7e6b42c3addae837e02bbb0f9360583bff
Aleksandr <kolapser@gmail.com>
2014-11-11 18:10:12 +0100

    Update tablica.c

--- Zadanie 3 Tasks/tablica.c
+++ Zadanie 3 Tasks/tablica.c
@@ -1,8 +1,8 @@
-#define MAX_TASKS_NUMBER  8
-typedef struct TabEl {
-	void *ptr;
-	int licznik;
-	int numer;
+#define MAX_TASKS_NUMBER  8 // ilosc elementów
+typedef struct TabEl {    //struktura
+	void *ptr;      // wskaźnik do funk
+	int licznik;    // po ilu przejściach timera będzie wywolana funkcja 
+	int numer;      // numer struktury dla robienia czegoś w funkcji (opc)
 };
 
 struct TabEl tablica[MAX_TASKS_NUMBER];	
