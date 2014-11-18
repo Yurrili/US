7a4d5b45b562d3c031dd8da801cc9983b7b16bf6
DonMat <acemateusza@gmail.com>
2014-11-18 20:01:32 +0100

    Biblioteka 0.025
    Dodany komentarz, wszystko sie kompiluje.

--- Zadanie 3 Tasks/cooperative_scheduler.c
+++ Zadanie 3 Tasks/cooperative_scheduler.c
@@ -1,34 +1,20 @@
+//Mateusz Utkala, Oleksandr Kuzhel, Klaudia Olejniczak
+
 #include "cooperative_scheduler.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#define MAX_NUMBER_OF_TASKS  8
 
-#ifndef MAX_NUMBER_OF_TASKS
-#define MAX_NUMBER_OF_TASKS  8 // ilosc elementow
-#endif
-
-
-static int counter1=0;
-static int wait = 1;
-
-typedef struct TabEl{    //struktura
-	task_ptr func_ptr;      // wskaznik do funk
-	int counter;		
+typedef struct TabEl{		 //struktura
+	task_ptr func_ptr;      // wskaznik do funkcji
+	int counter;			// ilosc zgloszen do wykonania
 	int priorytet;
-	int period;	// po ilu przejsciach timera bedzie wywolana funkcja
-	int ready;//czy do wykonania
-	void * params;
+	int period;				// po ilu przejsciach timera bedzie wywolana funkcja
+	int ready;				//czy gotowe do wykonania
+	void * params;			//parametry dla przekazanej funkcji
 } ;
 
 struct TabEl tablica[MAX_NUMBER_OF_TASKS];
-
-void keypak()
-{
-	counter1++;
-}
-void keypak1()
-{
-	counter1--;
-}
 
 void init_scheduler()
 {
@@ -41,11 +27,11 @@ void init_scheduler()
 
 void init_timer_irq()
 {
-	OCR0 = 250;//
-	TCCR0 |= (1 << WGM01)  | (0 << WGM00) ;
-	sei();	//wlacza przerwania
-	TIMSK |= (1 << OCIE0); // generowanie przerwania przy 250
-	TCCR0 |= (1<<CS01) | (1<< CS00);//ustawienie preskalera na 64
+	OCR0 = 250;
+	TCCR0 |= (1 << WGM01)  | (0 << WGM00) ;		//tryb CTC
+	sei();										//wlacza przerwania
+	TIMSK |= (1 << OCIE0);						// Output Compare Match Interrupt Enable 
+	TCCR0 |= (1<<CS01) | (1<< CS00);			//ustawienie preskalera na 64
 }
 
 
@@ -53,16 +39,16 @@ void shedule(){
 	int index;
 	for(index = 0; index < MAX_NUMBER_OF_TASKS ;index++)
 	{
-		if(tablica[index].ready != -1){// -1 <- w tablicy nie ma tutaj zadnego tasku
-			if(tablica[index].counter == 0){//zeby nam nie przyspieszalo liczenia
+		if(tablica[index].ready != -1){							// -1 to w tablicy nie ma tutaj zadnego tasku
+			if(tablica[index].counter == 0)	{					//zeby nam nie przyspieszalo liczenia przepisujemy ponownie wartosc period
 				tablica[index].counter = tablica[index].period;
 				} else {
 					
-				tablica[index].counter--;
+				tablica[index].counter--;						//gdy mozna zmniejszamy ilosc wywolan (oczekiwanie)
 				
-				if(tablica[index].counter == 0){
-					tablica[index].ready = 1;
-					wait=0; //ma zadanie do wykonania
+				if(tablica[index].counter == 0){				//gdy mina czas nalezy zaznaczyc zadania do wykonania, nie mozna dac 1 bo zadanie moglo byc kilka razy wywolane do pracy
+					tablica[index].ready++;
+					//wait=0; //ma zadanie do wykonania
 				}
 			}
 		}
@@ -70,7 +56,7 @@ void shedule(){
 	}
 }
 
-void AddTask(int priority,int period, task_ptr func_ptr, void *params){
+void AddTask(int priority,int period, task_ptr func_ptr, void *params){	//tablica[priorytet] przechowuje dane zadanie z okreslonym priorytetem
 	tablica[priority].func_ptr = func_ptr;
 	tablica[priority].period = period; 
 	tablica[priority].counter = period;
@@ -79,27 +65,26 @@ void AddTask(int priority,int period, task_ptr func_ptr, void *params){
 }
 
 void execute(){
-	
 	while(1){
 		int k;
 		
-			if(wait==0){//gdy w kolejce pojawia sie jakies zadanie do wykonania
+//			if(wait==0){//gdy w kolejce pojawia sie jakies zadanie do wykonania
 			
-				cli();	//blokuj przerwanie na czas przegladania zadania z taskami
+				cli();														//blokuj przerwanie na czas przegladania zadania z taskami
 			
 				for(k = 0; k < MAX_NUMBER_OF_TASKS; k++){
 					
-						if(tablica[k].ready == 1){//kiedy ready to wywoluje funkcje i ustawia conter znowu na okres odliczania
-							tablica[k].func_ptr(tablica[k].params);//tutaj powinno wywolac funkcje, ale raczej tego nie robi ^.^
-							tablica[k].ready = 0;
+						if(tablica[k].ready > 0){							//kiedy ready to wywoluje funkcje i ustawia conter znowu na okres odliczania
+							tablica[k].func_ptr(tablica[k].params);			// wywolaj funkcje z parametrami
+							tablica[k].ready --;							//zanotuj wywolanie
 						}
 					
-						if(k == MAX_NUMBER_OF_TASKS-1){
-							wait = 1;
-						}	
+//						if(k == MAX_NUMBER_OF_TASKS-1){
+//							wait = 1;
+//						}	
 				}
 			
 				sei();
 			}
-	}
+//	}
 }
--- Zadanie 3 Tasks/cooperative_scheduler.h
+++ Zadanie 3 Tasks/cooperative_scheduler.h
@@ -1,12 +1,14 @@
+//Mateusz Utkala, Oleksandr Kuzhel, Klaudia Olejniczak
 #ifndef __COOPERATIVE_SCHEDULER__
 #define __COOPERATIVE_SCHEDULER__
-typedef void (*task_ptr)(void *);
 
-void init_scheduler();
-void init_timer_irq();
-void shedule();
-void AddTask(int ,int , task_ptr , void* );
-void execute();
+typedef void (*task_ptr)(void *);				//zdefiniowany typ dla funkcji przekazywanych do shedule
+
+void init_scheduler();							//inicjalizacja tablicy dla zadan na -1 w ready
+void init_timer_irq();							//ustawienie timera 
+void shedule();									//funkcja wykonywana w przerwaniu
+void AddTask(int ,int , task_ptr , void* );		//dodawanie nowych taskow do tablicy
+void execute();									//glowna funkcja przegl¹dajaca i wykonujaca poszczegolne taski
 
 
 #endif
